576972b729895ebba1f8e2dd63baac121604bc26
vishnu244 <vishnunali244@gmail.com>
2022-05-05 17:00:22 +0530

    [Vishnu] using float

---
+++ Maximumofthree/Usingstrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximumofthree
+{
+    public class Usingstrings
+    {
+        public string MaxValueStrings(string a, string b, string c)
+        {
+            string max;
+            max = a;
+            if (b.Length.CompareTo(max.Length) > 0)
+                max = b;
+            if (c.Length.CompareTo(max.Length) > 0)
+                max = c;
+            return max;
+
+        }
+    }
+}
