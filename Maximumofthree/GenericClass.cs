5a5edc1b9de9960cd40b22d7adc53bbb74295404
vishnu244 <vishnunali244@gmail.com>
2022-05-06 15:42:00 +0530

    [vishnu] using generic class

---
+++ Maximumofthree/GenericClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximumofthree
+{
+    public class GenericClass 
+    {      
+        public static T MaxusingGenerics<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+            }
+            return a;
+        }
+    }
+}
